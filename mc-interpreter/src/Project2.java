447e0e8a688252e5d57f20f806642e8534a16ac4
Derrick <stjohndlee@gmail.com>
2018-03-27 23:33:13 -0400

    refactor into TokenScanner and Parser classes

--- mc-interpreter/src/Project2.java
+++ mc-interpreter/src/Project2.java
@@ -1,65 +1,10 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 
-/**
- * @author Derrick Lee <derrickdclee@gmail.com>
- */
 public class Project2 {
-	private String[][] parseTable;
-	private final int NUM_STATES = 38;
-	private final int NUM_TERMINALS = 18;
-	private final int NUM_VARS = 4;
-	
-	public Project2() {
-		this.parseTable = new String[NUM_STATES][NUM_TERMINALS + NUM_VARS];
-	}
-	
-	private void initParseTable() {
-		File f = new File("./parsedata/parsedata.txt");
-		String line = null;
-		
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(f));
-			int lineNum = -1;
-			while ( (line = br.readLine()) != null) {
-				if (lineNum == -1 || lineNum == NUM_STATES) {
-				
-				} else if (lineNum < NUM_STATES) {
-					String[] entries = line.split("&", -1); // negative number to retain trailing whitespaces
-					for (int i=1; i<entries.length; i++) {
-						this.parseTable[lineNum][i-1] = entries[i].isEmpty()? "err" : entries[i];
-					}
-				} else {
-					String[] entries = line.split("&", -1);
-					for (int i=1; i<entries.length; i++) {
-						this.parseTable[lineNum - (NUM_STATES + 1)][i + (NUM_TERMINALS - 1)] = 
-								entries[i].isEmpty()? "err" : entries[i];
-					}
-				}
-				lineNum++;
-			}
-			br.close();
-//			for (int i=0; i<this.parseTable.length; i++) {
-//				for (int j=0; j<this.parseTable[0].length; j++) {
-//					System.out.print(this.parseTable[i][j]);
-//					System.out.print(" ");
-//				}
-//				System.out.println();
-//			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
+
 	public static void main(String[] args) {
-		Project2 parser = new Project2();
-		parser.initParseTable();
+		Parser parser = new Parser();
+		
+		TokenScanner scanner = new TokenScanner();
 		
 	}
 
